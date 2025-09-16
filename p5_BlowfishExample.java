5c1a70e56681087c940c993e0a32603480851003
krishnamanojpvr <pvrkmsbunny@gmail.com>
2025-09-16 23:58:24 +0530

    cnslab

--- p5_BlowfishExample.java
+++ p5_BlowfishExample.java
@@ -7,14 +7,12 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
 public class p5_BlowfishExample {
-// Method to generate a secret key for Blowfish 
 
     public static SecretKey generateKey(int keySize) throws Exception {
         KeyGenerator keyGenerator = KeyGenerator.getInstance("Blowfish");
         keyGenerator.init(keySize); // keySize can be between 32 and 448 bits 
         return keyGenerator.generateKey();
     }
-// Method to encrypt data using the Blowfish algorithm 
 
     public static String encrypt(String plaintext, SecretKey key) throws Exception {
         Cipher cipher = Cipher.getInstance("Blowfish");
@@ -22,7 +20,6 @@ public class p5_BlowfishExample {
         byte[] encryptedBytes = cipher.doFinal(plaintext.getBytes());
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
-// Method to decrypt data using the Blowfish algorithm 
 
     public static String decrypt(String ciphertext, SecretKey key) throws Exception {
         Cipher cipher = Cipher.getInstance("Blowfish");
@@ -33,15 +30,11 @@ public class p5_BlowfishExample {
 
     public static void main(String[] args) {
         try {
-// Generate a secret key for Blowfish 
-            SecretKey secretKey = generateKey(128); // You can specify a key size between 32 and 448 bits   
-// Plain text to be encrypted 
+            SecretKey secretKey = generateKey(128); 
             String plaintext = "Hello, World!";
             System.out.println("Original Text: " + plaintext);
-// Encrypt the plain text 
             String encryptedText = encrypt(plaintext, secretKey);
             System.out.println("Encrypted Text: " + encryptedText);
-// Decrypt the encrypted text 
             String decryptedText = decrypt(encryptedText, secretKey);
             System.out.println("Decrypted Text: " + decryptedText);
         } catch (Exception e) {
